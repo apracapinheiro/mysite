f6b8ee3145daf2ce2015d0564a359c50a97dc4cc
81135998191 <81135998191@dti-desen15>
2020-11-09 17:42:46 -0300

    Listagem com Class Based Views

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) #  tres postagens em cada pagina
+    paginator = Paginator(object_list, 3)  # tres postagens em cada pagina
     page = request.GET.get('page')
 
     try:
@@ -23,3 +24,10 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, 
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
